40c8b319524e8b5b337fc60d6bd446eaa8abe7da
Steffen Rüttinger <devnull@snizzle.org>
2022-11-17 17:05:17 +0100

    separated service from actual uplaoding function

--- loguploader.py
+++ loguploader.py
@@ -3,98 +3,44 @@ import glob
 import os
 import zipfile
 from os.path import basename
-import sys
+from argparse import ArgumentParser
 from dropbox import public_link
-import time
-
-import win32serviceutil  # ServiceFramework and commandline helper
-import win32service  # Events
-import servicemanager  # Simple setup and logging
 
 
-class LumiLogUploadService:
-    """Luminosa Log Upload Service"""
-
-    def stop(self):
-        """Stop the service"""
-        self.running = False
-
-    def run(self):
-        """Main service loop. This is where work is done!"""
-        self.running = True
-        while self.running:
-            time.sleep(10)  # Important work
-            servicemanager.LogInfoMsg("Service running...")
-            upload()
-
-
-class LumiLogUploadServiceFramework(win32serviceutil.ServiceFramework):
-
-    _svc_name_ = "LumiLogUploadService"
-    _svc_display_name_ = "Luminosa Log Upload Service"
-
-    def SvcStop(self):
-        """Stop the service"""
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        self.service_impl.stop()
-        self.ReportServiceStatus(win32service.SERVICE_STOPPED)
-
-    def SvcDoRun(self):
-        """Start the service; does not return until stopped"""
-        self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
-        self.service_impl = LumiLogUploadService()
-        self.ReportServiceStatus(win32service.SERVICE_RUNNING)
-        # Run the service
-        self.service_impl.run()
-
-
-def init():
-    if len(sys.argv) == 1:
-        servicemanager.Initialize()
-        servicemanager.PrepareToHostSingle(LumiLogUploadServiceFramework)
-        servicemanager.StartServiceCtrlDispatcher()
-    else:
-        win32serviceutil.HandleCommandLine(LumiLogUploadServiceFramework)
-
-
-def upload():
+def upload(basepath):
 
     basepath = ""
 
     if not os.path.isdir(basepath):
         basepath = os.path.dirname(os.path.realpath(__file__))
-        servicemanager.LogInfoMsg(
-            f"No valid Log Directory given. Using: {basepath} instead."
-        )
-
-    servicemanager.LogInfoMsg(f"Log Directory: {basepath}")
-
+    returntxt = f"LogDir: {basepath}"
     try:
         nc = nextcloud_client.Client.from_public_link(public_link)
         filepattern = os.path.join(basepath, "*.pqlog")
-        servicemanager.LogInfoMsg(f"Log File Pattern: {filepattern}")
         for logfilename in glob.glob(filepattern):
-            servicemanager.LogInfoMsg(f"Found: {logfilename}")
             pre, ext = os.path.splitext(logfilename)
             zipfilename = pre + ".zip"
-            servicemanager.LogInfoMsg(f"Log File Name: {logfilename}")
             zipObj = zipfile.ZipFile(zipfilename, "w")
             zipObj.write(
                 logfilename, basename(logfilename), compress_type=zipfile.ZIP_DEFLATED
             )
             zipObj.close()
-            servicemanager.LogInfoMsg(f"Compressed: {logfilename} into: {zipfilename}")
+
             if nc.drop_file(zipfilename):
-                servicemanager.LogInfoMsg(f"Uploaded: {zipfilename}")
+                returntxt = f"Uploaded: {zipfilename}"
                 os.remove(logfilename)
-                servicemanager.LogInfoMsg(f"Deleted: {logfilename}")
             else:
-                servicemanager.LogInfoMsg(f"Upload Failed: {zipfilename}")
+                returntxt = f"Upload Failed: {zipfilename}"
                 os.remove(zipfilename)
-        servicemanager.LogInfoMsg("Done.")
     except:
-        servicemanager.LogInfoMsg(f"Cannot connect to {public_link}")
+        returntxt = f"Upload Failed"
+    return returntxt
 
 
 if __name__ == "__main__":
-    init()
+    parser = ArgumentParser()
+    parser.add_argument("dir", help="Log Directory", type=str, nargs="?", default="./")
+    args = parser.parse_args()
+    basepath = os.path.abspath(args.dir)
+
+    print(upload(basepath))
---
+++ loguploaderservice.py
@@ -0,0 +1,54 @@
+import time
+import win32serviceutil  # ServiceFramework and commandline helper
+import win32service  # Events
+import servicemanager  # Simple setup and logging
+import loguploader
+
+
+class LumiLogUploadService:
+    """Luminosa Log Upload Service"""
+
+    def stop(self):
+        """Stop the service"""
+        self.running = False
+
+    def run(self):
+        """Main service loop. This is where work is done!"""
+        self.running = True
+        while self.running:
+            time.sleep(10)  # Important work
+            # servicemanager.LogInfoMsg("Service running...")
+            servicemanager.LogInfoMsg(loguploader.upload())
+
+
+class LumiLogUploadServiceFramework(win32serviceutil.ServiceFramework):
+
+    _svc_name_ = "LumiLogUploadService"
+    _svc_display_name_ = "Luminosa Log Upload Service"
+
+    def SvcStop(self):
+        """Stop the service"""
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        self.service_impl.stop()
+        self.ReportServiceStatus(win32service.SERVICE_STOPPED)
+
+    def SvcDoRun(self):
+        """Start the service; does not return until stopped"""
+        self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
+        self.service_impl = LumiLogUploadService()
+        self.ReportServiceStatus(win32service.SERVICE_RUNNING)
+        # Run the service
+        self.service_impl.run()
+
+
+def init():
+    if len(sys.argv) == 1:
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(LumiLogUploadServiceFramework)
+        servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(LumiLogUploadServiceFramework)
+
+
+if __name__ == "__main__":
+    init()
