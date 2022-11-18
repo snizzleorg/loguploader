8774f8095d7afa7a7a764be14206f886590b950c
snizzleorg <universaldilettant@gmail.com>
2022-11-18 14:58:56 +0100

    Update getlumilogdir.py

--- getlumilogdir.py
+++ getlumilogdir.py
@@ -1,8 +1,7 @@
 import winpath
 import sys
-import winpath
 import os
 
 if sys.platform == "win32":  # WinPath will only work on Windows
     path = winpath.get_common_appdata()
-    print(os.path.join(path, Luminosa, Logs))
+    print(os.path.join(path, "Luminosa", "Logs"))
