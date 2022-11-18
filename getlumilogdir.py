5efd0eda02805a25d403142125b0d798e56add06
Steffen Rüttinger <devnull@snizzle.org>
2022-11-18 14:55:41 +0100

    added means to find luminosa log directory

--- getlumilogdir.py
+++ getlumilogdir.py
@@ -1,7 +1,8 @@
 import winpath
 import sys
 import winpath
+import os
 
 if sys.platform == "win32":  # WinPath will only work on Windows
     path = winpath.get_common_appdata()
-    print(path)
+    print(os.path.join(path, Luminosa, Logs))
