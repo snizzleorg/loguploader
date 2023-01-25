a9ea303e4e49f3f8a29a71a51ab8bd3bbc30fa9b
Steffen Rüttinger <devnull@snizzle.org>
2023-01-25 10:59:14 +0100

    remove temp files

--- getlumilogdir.py
+++
@@ -1,7 +0,0 @@
-import winpath
-import sys
-import os
-
-if sys.platform == "win32":  # WinPath will only work on Windows
-    path = winpath.get_common_appdata()
-    print(os.path.join(path, "Luminosa", "Logs"))
